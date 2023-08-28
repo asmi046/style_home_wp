e8459aedb3b61b8fd94040ee99a426ff4c8231d4
asmi046 <asmi046@gmail.com>
2023-08-28 20:35:19 +0300

    Этапы работ

--- _theme/template-parts/section-etapi-rabot.php
+++ _theme/template-parts/section-etapi-rabot.php
@@ -1,5 +1,46 @@
 <section id="section-etapi-rabot" class="section-etapi-rabot">
     <div class="_container">
-        <h2>section-etapi-rabot</h2>
+        <h2>Этапы работ</h2>
+        <div class="etapi_wrapper">
+            <div class="spl_line"></div>
+            <div class="etapi_line">
+                <div class="etap">
+                    <div class="circle"></div>
+                    <h3>Заявка</h3>
+                    <p>Оставляете заявку. Мы узнаем Ваши предпочтения и какой ремонт Вам нужен</p>
+                </div>
+
+                <div class="etap">
+                    <div class="circle"></div>
+                    <h3>Выезд на объект</h3>
+                    <p>Делаем замеры. Рассказываем, как проходит сотрудничество</p>
+                </div>
+
+                <div class="etap">
+                    <div class="circle"></div>
+                    <h3>Заключение договора</h3>
+                    <p>Составляем смету, график выполнения и оплаты работ. Подписываем договор</p>
+                </div>
+
+                <div class="etap">
+                    <div class="circle"></div>
+                    <h3>Закупка материалов</h3>
+                    <p>Закупаем и доставляем материалы на объект. Предоставляем все чеки и документы</p>
+                </div>
+
+                <div class="etap">
+                    <div class="circle"></div>
+                    <h3>Ремонт</h3>
+                    <p>Выполняем ремонтные работы согласно подписанному договору</p>
+                </div>
+
+                <div class="etap">
+                    <div class="circle"></div>
+                    <h3>Сдача объекта</h3>
+                    <p>Прием и сдача всех работ, клининг-уборка. Предоставление гарантии</p>
+                </div>
+                
+            </div>
+        </div>
     </div>
 </section>
