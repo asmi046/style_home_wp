43f25fd22037e4c4ec5c0c9d94c6aea202c1c54a
Ruvick <rudikov.web@ya.ru>
2023-08-29 00:41:58 +0300

    Правки карточек

--- _theme/template-parts/section-otzivi.php
+++ _theme/template-parts/section-otzivi.php
@@ -1,5 +1,24 @@
 <section id="section-otzivi" class="section-otzivi">
     <div class="_container">
-        <h2>section-otzivi</h2>
+        <h2>Отзывы наших клиентов</h2>
+
+				<div class="section-otzivi__inner">
+
+				<div class="section-otzivi__column">
+					<div class="section-otzivi__card-otzivi otzivi-card">
+						<div class="otzivi-card__header">
+
+						</div>
+						<div class="otzivi-card__body">
+
+						</div>
+						<div class="otzivi-card__btn-block">
+							<button type="button" class="otzivi-card__btn btn">Показать ещё</button>
+						</div>
+					</div>
+				</div>
+
+				</div>
+
     </div>
-</section>
+</section> 
