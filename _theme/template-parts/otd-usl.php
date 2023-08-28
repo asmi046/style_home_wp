6f85aca9635b72d5a575ff9366630824b9a6b895
asmi046 <asmi046@gmail.com>
2023-08-28 14:36:02 +0300

    Отдельные услуги

---
+++ _theme/template-parts/otd-usl.php
@@ -0,0 +1,13 @@
+<div class="otd_usl">
+    <div class="photo" data-uslname="<?php echo $args['name']?>"></div>
+    <h3><?php echo $args['name']?></h3>
+    <ul>
+        <li>Стяжка пола</li>
+        <li>Гидроизоляция</li>
+        <li>Монтаж теплого пола</li>
+        <li>Укладка напольного покрытия</li>
+    </ul>
+    <span class="price"><?php echo $args['price']?></span>
+    <p>без учета материалов</p>
+    <a href="#" class="btn">Рассчитать стоимость</a>
+</div>
--- _theme/template-parts/section-uslugi.php
+++ _theme/template-parts/section-uslugi.php
@@ -1,5 +1,17 @@
 <section id="section-uslugi" class="section-uslugi">
     <div class="_container">
-        <h2>section-uslugi</h2>
+        <h2>Отдельные услуги</h2>
+
+        <div class="otd_usl_blk">
+        
+            <?php get_template_part('template-parts/otd', 'usl', ["name" => "Ремонт полов", "price" => "от 260 руб/м²"]);?>
+            <?php get_template_part('template-parts/otd', 'usl', ["name" => "Ремонт потолков", "price" => "от 270 руб/м²"]);?>
+            <?php get_template_part('template-parts/otd', 'usl', ["name" => "Ремонт стен", "price" => "от 280 руб/м²"]);?>
+            <?php get_template_part('template-parts/otd', 'usl', ["name" => "Демонтажные работы", "price" => "от 290 руб/м²"]);?>
+            <?php get_template_part('template-parts/otd', 'usl', ["name" => "Сантехнические работы", "price" => "от 210 руб/м²"]);?>
+            <?php get_template_part('template-parts/otd', 'usl', ["name" => "Электромонтажные работы", "price" => "от 220 руб/м²"]);?>
+
+            
+        </div>
     </div>
 </section>
