5c0f1ef04eff8ae86c2233cadf293f97a42b5ee6
asmi046 <asmi046@gmail.com>
2023-08-28 18:42:17 +0300

    Наша команда

---
+++ _theme/template-parts/command-person.php
@@ -0,0 +1,9 @@
+<div class="command_person standart_roundet">
+    <div class="pictute_wrap">
+        <img src="<?php bloginfo("template_url")?>/img/comand/<?php echo $args['img']?>" alt="">
+    </div>
+    <div class="command_name_dolg">
+        <p class="name"><?php echo $args['name']?></p>
+        <p class="dolg"><?php echo $args['dolg']?></p>
+    </div>
+</div>
--- _theme/template-parts/section-komanda.php
+++ _theme/template-parts/section-komanda.php
@@ -1,5 +1,30 @@
 <section id="section-komanda" class="section-komanda">
     <div class="_container">
-        <h2>section-komanda</h2>
+        <h2>Наша команда</h2>
+        <div class="comand_blk">
+            <?php get_template_part('template-parts/command', "person", [
+                "name" => "Наталья Викторовна",
+                "dolg" => "Руководитель отдела дизайна",
+                "img" => "comand_1.jpg",
+            ]);?>
+            
+            <?php get_template_part('template-parts/command', "person", [
+                "name" => "Наталья Викторовна",
+                "dolg" => "Дизайнер",
+                "img" => "comand_2.jpg",
+            ]);?>
+            
+            <?php get_template_part('template-parts/command', "person", [
+                "name" => "Юрий Олегович",
+                "dolg" => "Руководитель отдела дизайна",
+                "img" => "comand_3.jpg",
+            ]);?>
+            
+            <?php get_template_part('template-parts/command', "person", [
+                "name" => "Наталья Викторовна",
+                "dolg" => "Руководитель отдела дизайна",
+                "img" => "comand_4.jpg",
+            ]);?>
+        </div>
     </div>
 </section>
