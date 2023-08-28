b7663fd97dfc43f6a3db0e03bbdbdb61596693f2
asmi046 <asmi046@gmail.com>
2023-08-28 20:16:10 +0300

    Вы защищены юридически

--- _theme/template-parts/section-uridiceskaya-zashita.php
+++ _theme/template-parts/section-uridiceskaya-zashita.php
@@ -1,5 +1,19 @@
 <section id="section-uridiceskaya-zashita" class="section-uridiceskaya-zashita">
     <div class="_container">
-        <h2>section-uridiceskaya-zashita</h2>
+        <h2>Вы защищены юридически</h2>
+        <div class="ur_zashita_blk standart_roundet">
+            <div class="img">
+
+            </div>
+            <div class="list">
+                <h3>Все гарантии <br/>зафиксированы в договоре</h3>
+                <ul class="preim_list">
+                    <li>Фиксированные расценки на услуги Цены на работы не вырастут в процессе ремонта</li>
+                    <li>Фиксированные сроки ремонта Ремонт будет окончен в согласованный срок ≈74% ремонтов сдаем раньше срока</li>
+                    <li>При ремонте с нашими материалами гарантийный срок 3 года Если с нашей работой что-то случится, мы исправим все бесплатно. Всего 1 гарантийный случай за последние 500 ремонтов</li>
+                </ul>
+            
+            </div>
+        </div>
     </div>
 </section>
