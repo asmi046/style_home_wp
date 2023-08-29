5b5ae3082e42994ec4568d7498d4588a5074a226
asmi046 <asmi046@gmail.com>
2023-08-30 00:49:17 +0300

    Оптимизация

--- _theme/index.php
+++ _theme/index.php
@@ -13,7 +13,7 @@
     <?php get_template_part('template-parts/section-remont-bez-nervov');?>
     <?php get_template_part('template-parts/section-uridiceskaya-zashita');?>
     <?php get_template_part('template-parts/section-etapi-rabot');?>
-    <?php //get_template_part('template-parts/section-direktor');?>
+    <?php get_template_part('template-parts/section-direktor');?>
     <?php get_template_part('template-parts/section-otzivi');?>
     <?php //get_template_part('template-parts/section-vizov-mastera');?>
     <?php //get_template_part('template-parts/section-faq');?>
