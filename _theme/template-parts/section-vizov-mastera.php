2c4cd288ea08325787f42c90a141897b2531ccff
asmi046 <asmi046@gmail.com>
2023-08-29 15:47:18 +0300

    Вызов мастера

--- _theme/template-parts/section-vizov-mastera.php
+++ _theme/template-parts/section-vizov-mastera.php
@@ -1,5 +1,39 @@
 <section id="section-vizov-mastera" class="section-vizov-mastera">
-    <div class="_container">
-        <h2>section-vizov-mastera</h2>
+    <div class="_container standart_roundet">
+        <div class="vm_form_blk">
+            <div class="left">
+                <h2>Бесплатно вызовите нашего специалиста в удобное время</h2>
+                <p class="sub_h">Заполните форму, мы перезвоним Вам <br/>и согласуем дату и время визита специалиста</p>
+                
+                <form action="">
+                    <label for="master_col_tel">
+                        <input type="tel" id="master_col_tel" name="phone" placeholder="Ваш телефон">
+                    </label>
+                    <button type="submit" class="btn">Перезвоните мне</button>
+                    <p class="snoska">Нажимая на кнопку, вы даете согласие на обработку персональных данных</p>
+                </form>
+
+            </div>
+            <div class="right">
+                <div class="wrap">
+                    <h3>К Вам на объект приедет опытный бригадир компании, который:</h3>
+                    <ul class="ul_in_blk">
+                        <li>Сделает точный замер</li>
+                        <li>Проконсультирует по видам работ и этапам</li>
+                        <li>Подскажет по выбору материалов</li>
+                        <li>Сориентирует по стоимости и срокам</li>
+                    </ul>
+                </div>
+
+                <div class="green_pl_srohno">
+                    <h3>Нужно срочно?</h3>
+                    <p>Проведем осмотр и консультацию по видеосвязи</p>
+                    <div class="phone_time">
+                        <a href="#">+7 495 287 67 25</a>
+                        <p class="time">с 10:00 до 21:00</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
