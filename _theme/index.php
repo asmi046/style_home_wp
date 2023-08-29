7c9e0038ad5a8430b69d63de35548b8cb6c48505
asmi046 <asmi046@gmail.com>
2023-08-30 01:01:02 +0300

    Вызов мастера моби

--- _theme/index.php
+++ _theme/index.php
@@ -15,7 +15,7 @@
     <?php get_template_part('template-parts/section-etapi-rabot');?>
     <?php get_template_part('template-parts/section-direktor');?>
     <?php get_template_part('template-parts/section-otzivi');?>
-    <?php //get_template_part('template-parts/section-vizov-mastera');?>
+    <?php get_template_part('template-parts/section-vizov-mastera');?>
     <?php //get_template_part('template-parts/section-faq');?>
     <?php //get_template_part('template-parts/section-moscow-card');?>
     <?php get_template_part('template-parts/section-footer');?>
