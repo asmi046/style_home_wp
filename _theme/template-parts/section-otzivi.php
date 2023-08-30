2af84553e03204e5613bff28793e20039092d3b2
Ruvick <rudikov.web@ya.ru>
2023-08-30 07:49:29 +0300

    Структура карточек отзывы

--- _theme/template-parts/section-otzivi.php
+++ _theme/template-parts/section-otzivi.php
@@ -7,9 +7,319 @@
 				<div class="section-otzivi__column">
 					<div class="section-otzivi__card-otzivi otzivi-card">
 						<div class="otzivi-card__header">
-
+							<div class="otzivi-card__header-row">
+							<div class="otzivi-card__header-logo">
+								<svg></svg>
+							</div>
+							<div class="otzivi-card__header-grade">5.0</div>
+							<div class="otzivi-card__header-rating rating-otzivi">
+								<svg></svg><svg></svg><svg></svg><svg></svg><svg></svg>
+							</div>
+							</div>
+							<div class="otzivi-card__header-line"></div>
+							<div class="otzivi-card__header-row">
+								<div class="otzivi-card__header-link-logo-text">Мы в</div>
+								<a href="#" class="otzivi-card__header-link-logo"><svg></svg></a>
+							</div>
 						</div>
 						<div class="otzivi-card__body">
+
+						<div class="otzivi-card__body-item">
+							<div class="otzivi-card__body-head">
+								<div class="otzivi-card__body-head-col">
+									<div class="otzivi-card__body-name"><svg></svg></div>
+									<div class="otzivi-card__body-name-text">Сугробова С.</div>
+								</div>
+								<div class="otzivi-card__body-head-col">
+									<div class="otzivi-card__body-rating rating-otzivi">
+									<svg></svg><svg></svg><svg></svg><svg></svg><svg></svg>
+								</div>
+							</div>
+							</div>
+							<div class="otzivi-card__body-descp">
+								Добрый вечер! Хочу поблагодарить компанию Домпрофи24. В моей квартире делали отделочные работы, очень благодарна рабочим, 
+								все слелали качественно и аккуратно. Рекомендую
+							</div>
+						</div>
+
+						<div class="otzivi-card__body-item">
+							<div class="otzivi-card__body-head">
+								<div class="otzivi-card__body-head-col">
+									<div class="otzivi-card__body-name"><svg></svg></div>
+									<div class="otzivi-card__body-name-text">Елена Осокина</div>
+								</div>
+								<div class="otzivi-card__body-head-col">
+									<div class="otzivi-card__body-rating rating-otzivi">
+									<svg></svg><svg></svg><svg></svg><svg></svg><svg></svg>
+								</div>
+							</div>
+							</div>
+							<div class="otzivi-card__body-descp">
+								Персонал очень вежливый. Внимательно относятся к своим заказчикам.
+							</div>
+						</div>
+
+						<div class="otzivi-card__body-item">
+							<div class="otzivi-card__body-head">
+								<div class="otzivi-card__body-head-col">
+									<div class="otzivi-card__body-name">T</div>
+									<div class="otzivi-card__body-name-text">Татьяна Матюшкина</div>
+								</div>
+								<div class="otzivi-card__body-head-col">
+									<div class="otzivi-card__body-rating rating-otzivi">
+									<svg></svg><svg></svg><svg></svg><svg></svg><svg></svg>
+								</div>
+							</div>
+							</div>
+							<div class="otzivi-card__body-descp">
+								Работала с этой компанией, помогали с оформлением бани. Созвонилась с менеджером, через пару часов приехал замерщик, с ним обсудили что и как. 
+								После менеджер помог выбрать материал, остановились на дубе - для пола и сосна- для стен. Через неделю всё доставили, в хорошем состоянии, 
+								выбрала сразу с покраской. Цены хорошие, персонал вежливый и приятный.
+							</div>
+						</div>
+
+						</div>
+						<div class="otzivi-card__btn-block">
+							<button type="button" class="otzivi-card__btn btn">Показать ещё</button>
+						</div>
+					</div>
+				</div>
+
+				<div class="section-otzivi__column">
+					<div class="section-otzivi__card-otzivi otzivi-card">
+						<div class="otzivi-card__header">
+							<div class="otzivi-card__header-row">
+							<div class="otzivi-card__header-logo">
+								<svg></svg>
+							</div>
+							<div class="otzivi-card__header-grade">5.0</div>
+							<div class="otzivi-card__header-rating rating-otzivi">
+								<svg></svg><svg></svg><svg></svg><svg></svg><svg></svg>
+							</div>
+							</div>
+							<div class="otzivi-card__header-line"></div>
+							<div class="otzivi-card__header-row">
+								<div class="otzivi-card__header-link-logo-text">Мы в</div>
+								<a href="#" class="otzivi-card__header-link-logo"><svg></svg></a>
+							</div>
+						</div>
+						<div class="otzivi-card__body">
+
+						<div class="otzivi-card__body-item">
+							<div class="otzivi-card__body-head">
+								<div class="otzivi-card__body-head-col">
+									<div class="otzivi-card__body-name"><svg></svg></div>
+									<div class="otzivi-card__body-name-text">Сугробова С.</div>
+								</div>
+								<div class="otzivi-card__body-head-col">
+									<div class="otzivi-card__body-rating rating-otzivi">
+									<svg></svg><svg></svg><svg></svg><svg></svg><svg></svg>
+								</div>
+							</div>
+							</div>
+							<div class="otzivi-card__body-descp">
+								Добрый вечер! Хочу поблагодарить компанию Домпрофи24. В моей квартире делали отделочные работы, очень благодарна рабочим, 
+								все слелали качественно и аккуратно. Рекомендую
+							</div>
+						</div>
+
+						<div class="otzivi-card__body-item">
+							<div class="otzivi-card__body-head">
+								<div class="otzivi-card__body-head-col">
+									<div class="otzivi-card__body-name"><svg></svg></div>
+									<div class="otzivi-card__body-name-text">Елена Осокина</div>
+								</div>
+								<div class="otzivi-card__body-head-col">
+									<div class="otzivi-card__body-rating rating-otzivi">
+									<svg></svg><svg></svg><svg></svg><svg></svg><svg></svg>
+								</div>
+							</div>
+							</div>
+							<div class="otzivi-card__body-descp">
+								Персонал очень вежливый. Внимательно относятся к своим заказчикам.
+							</div>
+						</div>
+
+						<div class="otzivi-card__body-item">
+							<div class="otzivi-card__body-head">
+								<div class="otzivi-card__body-head-col">
+									<div class="otzivi-card__body-name">T</div>
+									<div class="otzivi-card__body-name-text">Татьяна Матюшкина</div>
+								</div>
+								<div class="otzivi-card__body-head-col">
+									<div class="otzivi-card__body-rating rating-otzivi">
+									<svg></svg><svg></svg><svg></svg><svg></svg><svg></svg>
+								</div>
+							</div>
+							</div>
+							<div class="otzivi-card__body-descp">
+								Работала с этой компанией, помогали с оформлением бани. Созвонилась с менеджером, через пару часов приехал замерщик, с ним обсудили что и как. 
+								После менеджер помог выбрать материал, остановились на дубе - для пола и сосна- для стен. Через неделю всё доставили, в хорошем состоянии, 
+								выбрала сразу с покраской. Цены хорошие, персонал вежливый и приятный.
+							</div>
+						</div>
+
+						</div>
+						<div class="otzivi-card__btn-block">
+							<button type="button" class="otzivi-card__btn btn">Показать ещё</button>
+						</div>
+					</div>
+				</div>
+
+				<div class="section-otzivi__column">
+					<div class="section-otzivi__card-otzivi otzivi-card">
+						<div class="otzivi-card__header">
+							<div class="otzivi-card__header-row">
+							<div class="otzivi-card__header-logo">
+								<svg></svg>
+							</div>
+							<div class="otzivi-card__header-grade">5.0</div>
+							<div class="otzivi-card__header-rating rating-otzivi">
+								<svg></svg><svg></svg><svg></svg><svg></svg><svg></svg>
+							</div>
+							</div>
+							<div class="otzivi-card__header-line"></div>
+							<div class="otzivi-card__header-row">
+								<div class="otzivi-card__header-link-logo-text">Мы в</div>
+								<a href="#" class="otzivi-card__header-link-logo"><svg></svg></a>
+							</div>
+						</div>
+						<div class="otzivi-card__body">
+
+						<div class="otzivi-card__body-item">
+							<div class="otzivi-card__body-head">
+								<div class="otzivi-card__body-head-col">
+									<div class="otzivi-card__body-name"><svg></svg></div>
+									<div class="otzivi-card__body-name-text">Сугробова С.</div>
+								</div>
+								<div class="otzivi-card__body-head-col">
+									<div class="otzivi-card__body-rating rating-otzivi">
+									<svg></svg><svg></svg><svg></svg><svg></svg><svg></svg>
+								</div>
+							</div>
+							</div>
+							<div class="otzivi-card__body-descp">
+								Добрый вечер! Хочу поблагодарить компанию Домпрофи24. В моей квартире делали отделочные работы, очень благодарна рабочим, 
+								все слелали качественно и аккуратно. Рекомендую
+							</div>
+						</div>
+
+						<div class="otzivi-card__body-item">
+							<div class="otzivi-card__body-head">
+								<div class="otzivi-card__body-head-col">
+									<div class="otzivi-card__body-name"><svg></svg></div>
+									<div class="otzivi-card__body-name-text">Елена Осокина</div>
+								</div>
+								<div class="otzivi-card__body-head-col">
+									<div class="otzivi-card__body-rating rating-otzivi">
+									<svg></svg><svg></svg><svg></svg><svg></svg><svg></svg>
+								</div>
+							</div>
+							</div>
+							<div class="otzivi-card__body-descp">
+								Персонал очень вежливый. Внимательно относятся к своим заказчикам.
+							</div>
+						</div>
+
+						<div class="otzivi-card__body-item">
+							<div class="otzivi-card__body-head">
+								<div class="otzivi-card__body-head-col">
+									<div class="otzivi-card__body-name">T</div>
+									<div class="otzivi-card__body-name-text">Татьяна Матюшкина</div>
+								</div>
+								<div class="otzivi-card__body-head-col">
+									<div class="otzivi-card__body-rating rating-otzivi">
+									<svg></svg><svg></svg><svg></svg><svg></svg><svg></svg>
+								</div>
+							</div>
+							</div>
+							<div class="otzivi-card__body-descp">
+								Работала с этой компанией, помогали с оформлением бани. Созвонилась с менеджером, через пару часов приехал замерщик, с ним обсудили что и как. 
+								После менеджер помог выбрать материал, остановились на дубе - для пола и сосна- для стен. Через неделю всё доставили, в хорошем состоянии, 
+								выбрала сразу с покраской. Цены хорошие, персонал вежливый и приятный.
+							</div>
+						</div>
+
+						</div>
+						<div class="otzivi-card__btn-block">
+							<button type="button" class="otzivi-card__btn btn">Показать ещё</button>
+						</div>
+					</div>
+				</div>
+
+				<div class="section-otzivi__column">
+					<div class="section-otzivi__card-otzivi otzivi-card">
+						<div class="otzivi-card__header">
+							<div class="otzivi-card__header-row">
+							<div class="otzivi-card__header-logo">
+								<svg></svg>
+							</div>
+							<div class="otzivi-card__header-grade">5.0</div>
+							<div class="otzivi-card__header-rating rating-otzivi">
+								<svg></svg><svg></svg><svg></svg><svg></svg><svg></svg>
+							</div>
+							</div>
+							<div class="otzivi-card__header-line"></div>
+							<div class="otzivi-card__header-row">
+								<div class="otzivi-card__header-link-logo-text">Мы в</div>
+								<a href="#" class="otzivi-card__header-link-logo"><svg></svg></a>
+							</div>
+						</div>
+						<div class="otzivi-card__body">
+
+						<div class="otzivi-card__body-item">
+							<div class="otzivi-card__body-head">
+								<div class="otzivi-card__body-head-col">
+									<div class="otzivi-card__body-name"><svg></svg></div>
+									<div class="otzivi-card__body-name-text">Сугробова С.</div>
+								</div>
+								<div class="otzivi-card__body-head-col">
+									<div class="otzivi-card__body-rating rating-otzivi">
+									<svg></svg><svg></svg><svg></svg><svg></svg><svg></svg>
+								</div>
+							</div>
+							</div>
+							<div class="otzivi-card__body-descp">
+								Добрый вечер! Хочу поблагодарить компанию Домпрофи24. В моей квартире делали отделочные работы, очень благодарна рабочим, 
+								все слелали качественно и аккуратно. Рекомендую
+							</div>
+						</div>
+
+						<div class="otzivi-card__body-item">
+							<div class="otzivi-card__body-head">
+								<div class="otzivi-card__body-head-col">
+									<div class="otzivi-card__body-name"><svg></svg></div>
+									<div class="otzivi-card__body-name-text">Елена Осокина</div>
+								</div>
+								<div class="otzivi-card__body-head-col">
+									<div class="otzivi-card__body-rating rating-otzivi">
+									<svg></svg><svg></svg><svg></svg><svg></svg><svg></svg>
+								</div>
+							</div>
+							</div>
+							<div class="otzivi-card__body-descp">
+								Персонал очень вежливый. Внимательно относятся к своим заказчикам.
+							</div>
+						</div>
+
+						<div class="otzivi-card__body-item">
+							<div class="otzivi-card__body-head">
+								<div class="otzivi-card__body-head-col">
+									<div class="otzivi-card__body-name">T</div>
+									<div class="otzivi-card__body-name-text">Татьяна Матюшкина</div>
+								</div>
+								<div class="otzivi-card__body-head-col">
+									<div class="otzivi-card__body-rating rating-otzivi">
+									<svg></svg><svg></svg><svg></svg><svg></svg><svg></svg>
+								</div>
+							</div>
+							</div>
+							<div class="otzivi-card__body-descp">
+								Работала с этой компанией, помогали с оформлением бани. Созвонилась с менеджером, через пару часов приехал замерщик, с ним обсудили что и как. 
+								После менеджер помог выбрать материал, остановились на дубе - для пола и сосна- для стен. Через неделю всё доставили, в хорошем состоянии, 
+								выбрала сразу с покраской. Цены хорошие, персонал вежливый и приятный.
+							</div>
+						</div>
 
 						</div>
 						<div class="otzivi-card__btn-block">
